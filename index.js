479d5a9d9696bf767a10686c4cdddfaa8bd85e4a
imsmitgandhi <smitgandhi2001@gmail.com>
2022-04-03 17:18:15 +0530

    sql -db

---
+++ index.js
@@ -0,0 +1,23 @@
+const calc = require("./calc") //import
+
+calc.addition(10,10)
+calc.substraction(10,1)
+calc.muitiplication(10,3)
+
+
+
+
+
+
+// console.log("hello")
+
+// a=10
+// console.log(a);
+// a="royal"
+// console.log(a);
+// function add(a,b) {
+//      c=a+b
+//      console.log("addition = ",c);
+// }
+
+// add(10,20)
